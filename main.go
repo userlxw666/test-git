89f5d3e6a4b3ba09f951a50b630327bb133939fe
userlxw666 <lxw1378045191@163.com>
2023-09-10 19:59:17 +0800

    bug fix commit3

--- main.go
+++ main.go
@@ -6,6 +6,9 @@ func main() {
 	fmt.Println("hi golang")
 	fmt.Println("hi golang v2")
 	fmt.Println("hi golang v3")
-	fmt.Println("bug-fix modify...")
+	fmt.Println("bug-fix modify1...")
+	fmt.Println("bug-fix modify2...")
+	fmt.Println("bug-fix modify3...")
+	fmt.Println("bug-fix modify4...")
 	fmt.Println("bug-fix more...")
 }
