ef27b631544bb24e8955d3b1fdc1ac5ce4a1c574
userlxw666 <lxw1378045191@163.com>
2023-09-10 20:00:57 +0800

    master commit

--- main.go
+++ main.go
@@ -6,6 +6,9 @@ func main() {
 	fmt.Println("hi golang")
 	fmt.Println("hi golang v2")
 	fmt.Println("hi golang v3")
-	fmt.Println("bug-fix modify...")
+	fmt.Println("master bug-fix modify1...")
+	fmt.Println("master bug-fix modify2...")
+	fmt.Println("master bug-fix modify3...")
+	fmt.Println("master bug-fix modify4...")
 	fmt.Println("bug-fix more...")
 }
